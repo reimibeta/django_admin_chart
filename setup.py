4f73effc25c35f85f45538050b4af294e7b4bce0
reimi <65539218+reimibeta@users.noreply.github.com>
2023-06-08 16:41:54 +0700

    Initial commit

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='django_admin_chart',
+    version='1.0.0',
+    description='django admin chart',
+    long_description='django admin chart',
+    author='ReimiBeta',
+    author_email='reimi846@gmail.com',
+    url='https://github.com/reimibeta/django_html_render',
+    license='MIT',
+    packages=find_packages(),
+    # py_modules=['image_compress',],
+    install_requires=[
+        # other dependencies
+        'Django==4.1.7'
+    ],
+    # other optional arguments
+)
