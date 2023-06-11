f29931ac34c7e2d082febf7c4a353dfe3b6c9e6c
reimibeta <65539218+reimibeta@users.noreply.github.com>
2023-06-11 22:17:34 +0700

    .

--- example/chart_test/admin.py
+++ example/chart_test/admin.py
@@ -16,9 +16,10 @@ class ChartTestAdmin(admin.ModelAdmin):
     # change_list_template = 'admin/supplier_statistics.html'
     def changelist_view(self, request, extra_context=None):
         extra_context = extra_context or {}
-        extra_context['url1'] = '/view/chart/tests/test_chart/'
-        extra_context['url2'] = '/view/chart/tests/test_chart/'
+        extra_context['url'] = '/view/chart/tests/test_chart/'
+        # extra_context['url2'] = '/view/chart/tests/test_chart/'
         extra_context['url_filter'] = '/view/chart/tests/test_chart_filter/filter-options/'
+        extra_context['select_filter'] = 'value'
         self.change_list_template = 'django_admin_chart/one_bar_chart.html'
         # type_chart = duel_bar_chart, duel_pie_chart, duel_one_bar_one_pie, bar_chart, pie_chart
         return super().changelist_view(request, extra_context=extra_context)
--- example/chart_test/statistics.py
+++ example/chart_test/statistics.py
@@ -16,19 +16,19 @@ class TestStatisticsData(StatisticsData):
             .values('year').order_by('-year').distinct()
         years = [data['year'] for data in grouped_data]
 
-        # suppliers = ['']
-        # ss = Supplier.objects.all()
-        # for s in ss:
-        #     suppliers.append(s.name)
+        select_filters = ['']
+        ss = ChartTest.objects.all()
+        for s in ss:
+            select_filters.append(s.value)
         return {
             'years': years,
-            # 'suppliers': suppliers
+            'values': select_filters
         }
 
     def get_data(self, *args, **kwargs):
         data = ChartTest.objects.filter(date__year=kwargs.get("year"))  # .filter(status='COMPLETED')
-        # if 'supplier' in kwargs and kwargs.get("supplier") != "":
-        #     data = data.filter(supply_product__supplier__name=kwargs.get("supplier"))
+        if 'value' in kwargs and kwargs.get("value") != "":
+            data = data.filter(value=kwargs.get("value"))
         grouped_data = data.annotate(month=ExtractMonth('date')) \
             .values('month') \
             .annotate(amount=Sum('digit')) \
@@ -73,9 +73,10 @@ class TestStatisticsDataView(StatisticsChart):
     # @staff_member_required
     def get_data_chart(self, request):
         year = request.GET['year']
+        value = request.GET['value']
         data_request_dict = self.chart.get_year_dict()
         # print(year)
-        for data in self.ss.get_data(year=year):
+        for data in self.ss.get_data(year=year, value=value):
             data_request_dict[self.chart.months[data['month'] - 1]] = round(data['amount'], 2)  # r = revenue
         # print(self.ss.get_data(year=year))
         # print(data_request_dict)
