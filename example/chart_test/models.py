c3b6abdffa099df0679747730af749e9b76430af
Reimi <65539218+reimibeta@users.noreply.github.com>
2025-12-22 11:41:31 +0700

    .

--- example/chart_test/models.py
+++ example/chart_test/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class ChartTest(models.Model):
     date = models.DateField(blank=True, null=True)
     value = models.TextField(blank=True, null=True)
-    digit = models.IntegerField(default=0)
+    digit = models.DecimalField(default=0, max_digits=10, decimal_places=2)
 
     def __str__(self):
         return "{}".format(self.value)
